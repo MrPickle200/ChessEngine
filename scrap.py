f0ccb52113a3f6bca3d1a588c6e309b5783c93bf
Dao Viet <daothevietnb123@gmail.com>
2025-07-19 23:13:47 +0700

    Add Generation_pawns_move and Generation_knights_move for black and white.

--- scrap.py
+++ scrap.py
@@ -1,39 +1,28 @@
-white_pawns   = 0x000000000000FF00
-white_rooks   = 0x0000000000000081
-white_knights = 0x0000000000000042
-white_bishops = 0x0000000000000024
-white_queen   = 0x0000000000000008
-white_king    = 0x0000000000000010
-
-black_pawns   = 0x00FF000000000000
-black_rooks   = 0x8100000000000000
-black_knights = 0x4200000000000000
-black_bishops = 0x2400000000000000
-black_queen   = 0x0800000000000000
-black_king    = 0x1000000000000000
-
-white_occupancy = (white_pawns | white_rooks | white_knights |
-                   white_bishops | white_queen | white_king)
-
-black_occupancy = (black_pawns | black_rooks | black_knights |
-                   black_bishops | black_queen | black_king)
-
-all_occupancy = white_occupancy | black_occupancy
+from Piece import *
 
 bitboards = {
-    'P': white_pawns,
-    'N': white_knights,
-    'B': white_bishops,
-    'R': white_rooks,
-    'Q': white_queen,
-    'K': white_king,
-    'p': black_pawns,
-    'n': black_knights,
-    'b': black_bishops,
-    'r': black_rooks,
-    'q': black_queen,
-    'k': black_king,
-}
+    white_pawns.symbol: white_pawns,
+    white_knights.symbol: white_knights,
+    white_bishops.symbol: white_bishops,
+    white_rooks.symbol: white_rooks,
+    white_queen.symbol: white_queen,
+    white_king.symbol: white_king,
+    black_pawns.symbol: black_pawns,
+    black_knights.symbol: black_knights,
+    black_bishops.symbol: black_bishops,
+    black_rooks.symbol: black_rooks,
+    black_queen.symbol: black_queen,
+    black_king.symbol: black_king,
+} 
+
+def print_bitboard(bb):
+    for rank in range(7, -1, -1):
+        row = ""
+        for file in range(8):
+            sq = rank * 8 + file
+            row += '1 ' if (bb >> sq) & 1 else '. '
+        print(row)
+    print("")
 
 def print_board(bitboards:dict) -> None:
     board = ['.'] * 64
@@ -41,7 +30,7 @@ def print_board(bitboards:dict) -> None:
     # Fill in the board
     for piece, bb in bitboards.items():
         for square in range(64):
-            if (bb >> square) & 1:
+            if (bb.bitboard >> square) & 1:
                 board[square] = piece
 
     # Print the board top-down
@@ -55,52 +44,6 @@ def print_board(bitboards:dict) -> None:
         print(row)
     print("  +-----------------+")
     print("    a b c d e f g h")
-
-
-def get_piece_on_square(bitboards:dict, square:int) -> str:
-    for piece, bb in bitboards.items():
-        if (bb >> square) & 1:
-            return piece
-    return "."
-
-def set_piece_on_square(square:int, piece:str) -> None:
-    global bitboards
-    bitboards[piece] |= 1 << square 
-
-def clear_piece_on_square(square:int) -> None:
-    global bitboards
-    for piece in bitboards.keys():
-        bitboards[piece] &= ~(1 << square)
-
-def check_consistency(bitboards:dict) -> None:
-    for square in range(64):
-        count = 0
-        for bb in bitboards.values():
-            if (bb >> square) & 1:
-                count += 1
-        if count > 1:
-            print(f"⚠️  Square {square} has multiple pieces!")
-
-def move_piece(start_pos:str, target_pos:str) -> None:
-    global bitboards
-    start = convert_pos_to_idx(start_pos)
-    target = convert_pos_to_idx(target_pos)
-
-    moved_piece = None
-
-    for piece, bb in bitboards.items():
-        if (bb >> start) & 1:
-            moved_piece = piece
-            bitboards[piece] &= ~(1 << start)
-            break
-    if moved_piece is None:
-        print("No piece at " + start_pos)
-        return
-    for piece, bb in bitboards.items():
-        if (bb >> target) & 1:
-            bitboards[piece] &= ~(1 << target)
-    
-    bitboards[moved_piece] |= (1 << target)
 
 def convert_pos_to_idx(pos:str) -> int:
     table = {
@@ -118,7 +61,129 @@ def convert_pos_to_idx(pos:str) -> int:
             table[char + str(i)] = table[char + str(i - 1)] + 8
     return table[pos]
 
-check_consistency(bitboards)
+def convert_idx_to_pos(idx:int) -> str:
+    table = {
+        "a1":0,
+        "b1":1,
+        "c1":2,
+        "d1":3,
+        "e1":4,
+        "f1":5,
+        "g1":6,
+        "h1":7
+             }
+    for i in range(2,9):
+        for char in ['a','b','c','d','e','f','g','h']:
+            table[char + str(i)] = table[char + str(i - 1)] + 8
+    for key, value in table.items():
+        if idx == value:
+            return key
+        
+def generate_white_pawns_move(white_pawns:Piece, all_occupancy:int, black_occupancy:int) -> list:
+    all_moves = []
+    for i in range(64):
+        if white_pawns.is_on_square(i):
+            #Single push
+            from_sq = i
+            one_step = i + 8
+            if one_step < 64 and is_square_empty(all_occupancy, one_step): # (all_occupancy >> one_step) & 1 == 0 use to check if the targeted square is empty
+                all_moves.append((convert_idx_to_pos(from_sq), convert_idx_to_pos(one_step)))
+                
+                #Double push if the pawn is in rank 2
+                if 8 <= i <= 15:
+                    two_step = i + 16
+                    if is_square_empty(all_occupancy, two_step):
+                        all_moves.append((convert_idx_to_pos(from_sq), convert_idx_to_pos(two_step)))
+            # Capture left (<< 7)
+            if from_sq % 8 != 0: # Not in rank A
+                left_capture = i + 7
+                if left_capture < 64 and is_square_occupied_by_black(black_occupancy, left_capture):
+                    all_moves.append((convert_idx_to_pos(from_sq), convert_idx_to_pos(left_capture)))
+            # Capture riight (<< 9)
+            if from_sq % 7 != 0: # Not in rank H
+                right_capture = i + 9
+                if right_capture < 64 and is_square_occupied_by_black(black_occupancy, right_capture):
+                    all_moves.append((convert_idx_to_pos(from_sq), convert_idx_to_pos(right_capture)))
+
+    return all_moves
+
+def generate_black_pawns_move(black_pawns:Piece, all_occupancy:int, white_occupancy:int) -> list:
+    all_moves = []
+    for i in range(64):
+        if black_pawns.is_on_square(i):
+            #Single push
+            from_sq = i
+            one_step = i - 8
+            if one_step >= 0 and is_square_empty(all_occupancy, one_step): # (all_occupancy << one_step) & 1 == 0 use to check if the targeted square is empty
+                all_moves.append((convert_idx_to_pos(from_sq), convert_idx_to_pos(one_step)))
+                
+                #Double push if the pawn is in rank 2
+                if 48 <= i <= 55:
+                    two_step = i - 16
+                    if is_square_empty(all_occupancy, two_step):
+                        all_moves.append((convert_idx_to_pos(from_sq), convert_idx_to_pos(two_step)))
+            # Capture left (>> 7)
+            if from_sq % 8 != 0: # Not in rank A
+                left_capture = i + 7
+                if left_capture < 64 and is_square_occupied_by_white(white_occupancy, left_capture):
+                    all_moves.append((convert_idx_to_pos(from_sq), convert_idx_to_pos(left_capture)))
+            # Capture riight (>> 9)
+            if from_sq % 7 != 0: # Not in rank H
+                right_capture = i + 9
+                if right_capture < 64 and is_square_occupied_by_white(white_occupancy, right_capture):
+                    all_moves.append((convert_idx_to_pos(from_sq), convert_idx_to_pos(right_capture)))
+
+    return all_moves
+
+def generate_white_knight_move(white_knights:Piece, all_occupancy:int, black_occupancy:int) -> list:
+    all_moves = []
+    OFFSET: list[int] = [-17, -15, -10, -6, 6, 10, 15, 17]
+    for from_sq in range(64):
+        if not white_knights.is_on_square(from_sq):
+            continue
+
+        file = from_sq % 8
+        for offset in OFFSET:
+            to_sq = from_sq + offset
+            if to_sq < 0 or to_sq >= 64:
+                continue
+
+            file_diff = abs(file - (to_sq % 8))
+            if file_diff > 2: # Knight's move never change file over 2
+                continue
+            if is_square_empty(all_occupancy, to_sq) or is_square_occupied_by_black(black_occupancy, to_sq):
+                all_moves.append((convert_idx_to_pos(from_sq), convert_idx_to_pos(to_sq)))
+
+    return all_moves
+
+def generate_black_knight_move(black_knights:Piece, all_occupancy:int, white_occupancy:int) -> list:
+    all_moves = []
+    OFFSET: list[int] = [-17, -15, -10, -6, 6, 10, 15, 17]
+    for from_sq in range(64):
+        if not black_knights.is_on_square(from_sq):
+            continue
+
+        file = from_sq % 8
+        for offset in OFFSET:
+            to_sq = from_sq + offset
+            if to_sq < 0 or to_sq >= 64:
+                continue
+
+            file_diff = abs(file - (to_sq % 8))
+            if file_diff > 2: # Knight's move never change file over 2
+                continue
+            if is_square_empty(all_occupancy, to_sq) or is_square_occupied_by_white(white_occupancy, to_sq):
+                all_moves.append((convert_idx_to_pos(from_sq), convert_idx_to_pos(to_sq)))
+
+    return all_moves
+
+def is_square_empty(occupancy:int, square:int) -> bool:
+    return ((occupancy >> square) & 1) == 0
+def is_square_occupied_by_white(white_occupancy:int, square:int) -> bool:
+    return (white_occupancy << square) & 1 == 1
+def is_square_occupied_by_black(black_occupancy:int, square:int) -> bool:
+    return (black_occupancy >> square) & 1 == 1
+
 print_board(bitboards)
-move_piece("a2","a4")
-print_board(bitboards)
+print(generate_white_knight_move(white_knights, all_occupancy, black_occupancy))
+print(generate_black_knight_move(black_knights, all_occupancy, white_occupancy))
